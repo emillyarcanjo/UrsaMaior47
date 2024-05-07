e610c294bb52aba65a27ad198751df9c0b62460d
IzabelaMarina <83678692+IzabelaMarina@users.noreply.github.com>
2024-05-07 19:27:17 -0300

    Muda posição camera e adiciona controle por setas

--- Assets/Scripts/MoveRocket.cs
+++ Assets/Scripts/MoveRocket.cs
@@ -14,21 +14,21 @@ public class MoveRocket : MonoBehaviour
     // Update is called once per frame  
     void Update()  
     {  
-        if (Input.GetKey(KeyCode.A))  
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))  
         {
-            transform.Translate(0.0f, -0.1f, 0f);    
+            transform.Translate(Vector3.left * 0.1f, Space.Self);    
         }  
-        if (Input.GetKey(KeyCode.D))  
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))  
         {  
-            transform.Translate(0.0f, 0.1f, 0f);  
+            transform.Translate(Vector3.right * 0.1f, Space.Self);  
         }  
-        if (Input.GetKey(KeyCode.S))  
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))  
         {  
-            transform.Translate(0.1f, 0f, 0f);  
+            transform.Translate(Vector3.back*0.1f, Space.Self);  
         }  
-        if (Input.GetKey(KeyCode.W))  
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))  
         {  
-            transform.Translate(-0.1f, 0f, 0f);    
+            transform.Translate(Vector3.forward*0.1f, Space.Self);    
         }  
     }  
 }  
