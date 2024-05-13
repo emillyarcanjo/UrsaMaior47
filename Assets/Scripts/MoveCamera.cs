30c66bf7598df714ba144a3942d65d700d59b5c3
Lucas Ramos <lucasret22@gmail.com>
2024-05-13 20:46:46 -0300

    add xbox controller

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -1,40 +1,41 @@
-using System.Collections;  
-using System.Collections.Generic;  
-using UnityEngine;  
-  
-public class MoveCamera : MonoBehaviour  
-{  
-    public GameObject Rocket;
-    private Vector3 offset;
-    public float smoothTime = 0.2f;
-    private Vector3 velocity = Vector3.zero;
-    void Start()  
-    {  
-        offset = transform.position - Rocket.transform.position;
-    }  
-  
-    void Update()  
-    {  
-        if (Input.GetKey(KeyCode.A))  
-        {  
-            transform.RotateAround(Rocket.transform.position, Rocket.transform.up, -0.5f);    
-        }
-        if (Input.GetKey(KeyCode.D))  
-        {  
-            transform.RotateAround(Rocket.transform.position, Rocket.transform.up, 0.5f);   
-        }
-        if (Input.GetKey(KeyCode.W))  
-        {  
-            transform.RotateAround(Rocket.transform.position, Rocket.transform.forward, 0.5f);    
-        }
-        if (Input.GetKey(KeyCode.S))  
-        {  
-            transform.RotateAround(Rocket.transform.position, Rocket.transform.forward, -0.5f);    
-        }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveVector;
+    Vector2 rotateVector;
+
+    public float moveSpeed = 8f;
+    public float rotationSpeed = 100f;
+
+    public void InputPlayer(InputAction.CallbackContext _context)
+    {
+        moveVector = _context.ReadValue<Vector2>();
     }
 
-    void LateUpdate(){
-        Vector3 targetPosition = Rocket.transform.position + Rocket.transform.localRotation*offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    public void InputRotate(InputAction.CallbackContext _context)
+    {
+        rotateVector = _context.ReadValue<Vector2>();
     }
-}  
+
+    private void Update()
+    {
+        // Handle movement
+        Vector3 movement = new Vector3(moveVector.x, 0, moveVector.y);
+        movement.Normalize();
+        transform.Translate(moveSpeed * movement * Time.deltaTime, Space.World);
+
+        // Handle rotation
+        float horizontalRotation = rotateVector.x * rotationSpeed * Time.deltaTime;
+        float verticalRotation = rotateVector.y * rotationSpeed * Time.deltaTime;
+
+        // Rotating around Y-axis for left/right rotation
+        transform.Rotate(0, horizontalRotation, 0, Space.World);
+
+        // Optionally, if you want to handle up/down rotation with the right analog stick (around the X-axis), you can uncomment the following line:
+        // transform.Rotate(-verticalRotation, 0, 0, Space.Self);
+    }
+}
