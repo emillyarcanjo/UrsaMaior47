659ec06f310eb8723c38520a4ca2b1935a1a8b93
Lucas Ramos <lucasret22@gmail.com>
2024-05-19 12:42:00 -0300

    Merge branch 'main' into lucas

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class MoveCamera : MonoBehaviour
 {
     Vector2 moveVector;
     Vector2 rotateVector;
 
     public float moveSpeed = 8f;
     public float rotationSpeed = 100f;
+
+    public GameObject Rocket;
+    private Vector3 offset;
+    public float smoothTime = 0.2f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()  
+    {  
+        offset = transform.position - Rocket.transform.position;
+    }  
 
     public void InputPlayer(InputAction.CallbackContext _context)
     {
@@ -24,6 +34,22 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
 
+if (Input.GetKey(KeyCode.A))  
+        {  
+            transform.RotateAround(Rocket.transform.position, Rocket.transform.up, -0.5f);    
+        }
+        if (Input.GetKey(KeyCode.D))  
+        {  
+            transform.RotateAround(Rocket.transform.position, Rocket.transform.up, 0.5f);   
+        }
+        if (Input.GetKey(KeyCode.W))  
+        {  
+            transform.RotateAround(Rocket.transform.position, Rocket.transform.forward, 0.5f);    
+        }
+        if (Input.GetKey(KeyCode.S))  
+        {  
+            transform.RotateAround(Rocket.transform.position, Rocket.transform.forward, -0.5f);    
+        }
         Vector3 movement = new Vector3(moveVector.x, 0, moveVector.y);
         movement.Normalize();
         transform.Translate(moveSpeed * movement * Time.deltaTime, Space.Self);
@@ -33,5 +59,11 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Rotate(0, horizontalRotation, 0, Space.World);
 
+    }
+
+
+    void LateUpdate(){
+        Vector3 targetPosition = Rocket.transform.position + Rocket.transform.localRotation*offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
