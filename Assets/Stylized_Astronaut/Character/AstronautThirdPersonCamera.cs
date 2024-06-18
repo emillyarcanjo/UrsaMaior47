ea7c56db2f828e91583ebe87cb158ce7365e8176
Emilly Arcanjo <emilly.ssa@gmail.com>
2024-06-18 01:14:57 -0300

    update camera

--- Assets/Stylized_Astronaut/Character/AstronautThirdPersonCamera.cs
+++ Assets/Stylized_Astronaut/Character/AstronautThirdPersonCamera.cs
@@ -7,8 +7,8 @@ namespace AstronautThirdPersonCamera
 {
     public class AstronautThirdPersonCamera : MonoBehaviour
     {
-        private const float Y_ANGLE_MIN = 0.0f;
-        private const float Y_ANGLE_MAX = 50.0f;
+        public float Y_ANGLE_MIN = -10.0f;
+        public float Y_ANGLE_MAX = 20.0f;
         public Transform lookAt;
         public Transform camTransform;
         public float distance = 80.0f;
